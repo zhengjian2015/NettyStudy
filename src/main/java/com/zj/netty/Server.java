6aaef2bbac2f30b51d10804f1523ff6c7ee263d8
郑健 <570470242@qq.com>
2020-01-21 17:28:38 +0800

    netty第一课

--- src/main/java/com/zj/netty/Server.java
+++ src/main/java/com/zj/netty/Server.java
@@ -1,16 +1,43 @@
 package com.zj.netty;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
 import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
 
 public class Server {
     public static void main(String[] args) throws IOException {
-        ServerSocket ss = new ServerSocket();
-        ss.bind(new InetSocketAddress(8888));
 
-        Socket s = ss.accept();
-        System.out.println("a client connect");
+        //迎接线程，类似大管家
+        EventLoopGroup bootGroup = new NioEventLoopGroup(1);
+        //服务员 处理线程
+        EventLoopGroup workerGroup = new NioEventLoopGroup(2);
+
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            ChannelFuture f = b.group(bootGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            System.out.println(ch);
+                        }
+                    }).bind(8888)
+                    .sync();
+
+            System.out.println("server started!");
+            //机器人  接收关闭命令 关闭线程
+            f.channel().closeFuture().sync(); //close() -> channelFuture
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+
+        }
     }
 }
